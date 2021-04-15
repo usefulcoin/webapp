e18c18ea69ee8462191b313b78eb6f21c1dea311
Sha <knott@Knotts-MacBook-Pro.local>
2021-04-14 20:08:24 -0400

    shrink loading logo

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -9,8 +9,8 @@ const SLoading = styled.div`
     background: url(${logo}) no-repeat;
     background-size: cover;
     background-position: center;
-    height:60vh;
-    width:40vh;
+    height:30vh;
+    width:20vh;
     margin: 0 auto;
     -webkit-animation: spin 4s infinite linear;
 `
